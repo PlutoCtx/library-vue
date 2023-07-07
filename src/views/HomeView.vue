c0b55db52c6f6dce3f5400c9c950d4472061e865
PlutoCtx <chentingxian195467@163.com>
2023-07-07 22:33:36 +0800

    temp

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,10 @@
 
         <!-- 搜索表单 -->
         <div style="margin-bottom: 20px">
-            <el-input style="width: 240px;" placeholder="请输入名称"></el-input>
-            <el-input style="width: 240px; margin-left: 5px" placeholder="请输入联系方式"></el-input>
-            <el-button style="margin-left: 5px" type="primary"><i class="el-icon-search"></i>搜索</el-button>
+            <el-input style="width: 240px;" placeholder="请输入名称" v-model="params.name"></el-input>
+            <el-input style="width: 240px; margin-left: 5px" placeholder="请输入联系方式" v-model="params.phone"></el-input>
+            <el-button style="margin-left: 5px" type="primary" @click="load"><i class="el-icon-search"></i>搜索</el-button>
+            <el-button style="margin-left: 5px" type="warning" @click="reset"><i class="el-icon-refresh"></i>重置</el-button>
         </div>
 
 
@@ -22,20 +23,30 @@
         <div style="margin-top: 20px">
             <el-pagination
                 background
-                :page-size="5"
+                :current-page="params.pageNum"
+                :page-size="params.pageSize"
                 layout="prev, pager, next"
-                :total="100">
+                :total="total">
             </el-pagination>
         </div>
     </div>
 </template>
 
 <script>
+import request from "@/utils/request";
+
 export default {
     name: 'HomeView',
     data() {
         return {
-            tableData: []
+            tableData: [],
+            total: 0,
+            params: {
+                pageNum: 1,
+                pageSize: 10,
+                name: '',
+                phone: ''
+            }
         }
     },
     created() {
@@ -43,10 +54,29 @@ export default {
     },
     methods: {
         load() {
-            fetch('http://localhost:9090/user/list').then(res => res.json()).then(res => {
-                console.log(res)
-                this.tableData = res
+            // fetch('http://localhost:9090/user/list').then(res => res.json()).then(res => {
+            //     console.log(res)
+            //     this.tableData = res
+            // })
+
+            request.get('/user/page',{
+                params: this.params
+            }).then(res => {
+                if (res.code === '200'){
+                    this.tableData = res.data.list
+                    this.total = res.data.total
+                }
             })
+        },
+
+        reset() {
+            this.params = {
+                pageNum: 1,
+                pageSize: 10,
+                name: '',
+                phone: ''
+            }
+            this.load()
         }
     }
 }
